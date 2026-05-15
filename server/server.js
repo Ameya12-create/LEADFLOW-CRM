03de3446bdf89d97a3133c58a2f9c0b9c409205f
Ameya Manoj Joshi <joshiameya21@gmail.com>
2026-05-15 16:27:47 +0530

    connected both frontend and backend

--- server/server.js
+++ server/server.js
@@ -10,10 +10,15 @@ dotenv.config()
 
 const app = express()
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}))
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://leadflow-crm-xi-bice.vercel.app",
+    ],
+    credentials: true,
+  })
+)
 
 app.use(express.json())
 
